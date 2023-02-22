21fb02a99c9b65890067d4308d8b291245204fb6
uservar <userfloat@protonmail.com>
2023-02-22 18:45:16 +0100

    Fix ball rotations. Use automatic hitbox values

--- rocketsimvisualizer/visualizer.py
+++ rocketsimvisualizer/visualizer.py
@@ -76,30 +76,30 @@ class Visualizer:
         self.w.show()
 
         # Add ground grid
-        gz = gl.GLGridItem()
-        gz.setSize(8192, 10240 + 880 * 2, 1)
-        gz.setSpacing(100, 100, 100)
-        self.w.addItem(gz)
+        grid_item = gl.GLGridItem()
+        grid_item.setSize(8192, 10240 + 880 * 2, 1)
+        grid_item.setSpacing(100, 100, 100)
+        self.w.addItem(grid_item)
 
-        # debug info
+        # text info
         self.text_item = gl.GLTextItem(pos=(0, 0, 60))
 
         self.default_edge_color = (1, 1, 1, 1)
 
         # Create stadium 3d model
         stadium_object = obj.OBJ(current_dir / "models/field_simplified.obj")
-        md = gl.MeshData(vertexes=stadium_object.vertices, faces=stadium_object.faces)
-        m4 = gl.GLMeshItem(meshdata=md, smooth=False, drawFaces=False, drawEdges=True,
-                           edgeColor=self.default_edge_color)
-        m4.rotate(90, 0, 0, 1)
-        self.w.addItem(m4)
+        stadium_md = gl.MeshData(vertexes=stadium_object.vertices, faces=stadium_object.faces)
+        stadium_mi = gl.GLMeshItem(meshdata=stadium_md, smooth=False, drawFaces=False, drawEdges=True,
+                                   edgeColor=self.default_edge_color)
+        stadium_mi.rotate(90, 0, 0, 1)
+        self.w.addItem(stadium_mi)
 
         # Create ball geometry
         ball_radius = self.arena.ball.get_radius() * 50
         ball_md = gl.MeshData.sphere(rows=8, cols=16, radius=ball_radius)
-        self.ball = gl.GLMeshItem(meshdata=ball_md, smooth=False, drawFaces=True, drawEdges=True,
-                                  edgeColor=self.default_edge_color, color=(0.1, 0.1, 0.1, 1))
-        self.w.addItem(self.ball)
+        self.ball_mi = gl.GLMeshItem(meshdata=ball_md, smooth=False, drawFaces=True, drawEdges=True,
+                                     edgeColor=self.default_edge_color, color=(0.1, 0.1, 0.1, 1))
+        self.w.addItem(self.ball_mi)
 
         # Create ground projection for the ball
         ball_proj_md = gl.MeshData.cylinder(rows=1, cols=16, length=0, radius=round(ball_radius))
@@ -111,38 +111,52 @@ class Visualizer:
         big_pad_md = gl.MeshData.cylinder(rows=1, cols=4, length=64, radius=160)
         small_pad_md = gl.MeshData.cylinder(rows=1, cols=4, length=64, radius=144)
 
-        self.boost_pads = []
+        self.pads_mi = []
         for pad in arena.get_boost_pads():
             pad_pos = pad.get_pos()
-            boost_md = big_boost_md if pad.is_big else small_boost_md
-            boost_mesh = gl.GLMeshItem(meshdata=boost_md, drawFaces=False, drawEdges=True,
-                                       edgeColor=self.default_edge_color)
-            boost_mesh.rotate(45, 0, 0, 1)
-            boost_mesh.translate(-pad_pos.x, pad_pos.y, pad_pos.z)
-            self.boost_pads.append(boost_mesh)
-            self.w.addItem(boost_mesh)
+            pad_md = big_pad_md if pad.is_big else small_pad_md
+            pad_mi = gl.GLMeshItem(meshdata=pad_md, drawFaces=False, drawEdges=True,
+                                   edgeColor=self.default_edge_color)
+            pad_mi.rotate(45, 0, 0, 1)
+            pad_mi.translate(-pad_pos.x, pad_pos.y, pad_pos.z)
+            self.pads_mi.append(pad_mi)
+            self.w.addItem(pad_mi)
 
         # Create car geometry
         car_object = obj.OBJ(current_dir / "models/Octane_decimated.obj")
         car_md = gl.MeshData(vertexes=car_object.vertices, faces=car_object.faces)
 
-        car_hitbox_object = obj.OBJ(current_dir / "models/OctaneHitbox.obj")
-        car_hitbox_md = gl.MeshData(vertexes=car_hitbox_object.vertices,
-                                    faces=car_hitbox_object.faces)
+        self.blue_color = (0, 0.4, 0.8, 1)
+        self.orange_color = (1, 0.2, 0.1, 1)
 
-        self.cars = []
+        self.cars_mi = []
         for car in arena.get_cars():
-            car_color = (0, 0.4, 0.8, 1) if car.team == 0 else (1, 0.2, 0.1, 1)
-            car_mesh = gl.GLMeshItem(meshdata=car_md, smooth=False,
-                                     drawFaces=True, drawEdges=True,
-                                     color=car_color, edgeColor=self.default_edge_color)
 
-            hitbox_mesh = gl.GLMeshItem(meshdata=car_hitbox_md, smooth=False,
-                                        drawFaces=False, drawEdges=True, color=car_color,
-                                        edgeColor=self.default_edge_color)
-            hitbox_mesh.setParentItem(car_mesh)
-            self.cars.append(car_mesh)
-            self.w.addItem(car_mesh)
+            car_color = self.blue_color if car.team == 0 else self.orange_color
+
+            car_mi = gl.GLMeshItem(meshdata=car_md, smooth=False,
+                                   drawFaces=True, drawEdges=True,
+                                   color=car_color, edgeColor=self.default_edge_color)
+
+            self.cars_mi.append(car_mi)
+            self.w.addItem(car_mi)
+
+            # hitbox
+            car_config = car.get_config()
+            hitbox_size = car_config.hitbox_size
+            dia_conv = 1 / math.sqrt(2)
+            hitbox_offset = car_config.hitbox_pos_offset
+            car_hitbox_md = gl.MeshData.cylinder(rows=1, cols=4, radius=(dia_conv, dia_conv))
+
+            hitbox_mi = gl.GLMeshItem(meshdata=car_hitbox_md, smooth=False,
+                                      drawFaces=False, drawEdges=True,
+                                      edgeColor=self.default_edge_color)
+
+            hitbox_mi.rotate(45, 0, 0, 1)
+            hitbox_mi.translate(0, 0, -0.5)  # by default cylinder origin z loc is at 0.5 length
+            hitbox_mi.scale(hitbox_size.x, hitbox_size.y, hitbox_size.z, local=False)
+            hitbox_mi.translate(-hitbox_offset.x, hitbox_offset.y, hitbox_offset.z, local=False)
+            hitbox_mi.setParentItem(car_mi)
 
         # index of the car we control/spectate
         self.car_index = 0
@@ -160,9 +174,9 @@ class Visualizer:
         self.update()
 
     def get_cam_targets(self):
-        if not self.cars:
-            return [self.ball]
-        targets = self.cars + [self.ball]
+        if not self.cars_mi:
+            return [self.ball_mi]
+        targets = self.cars_mi + [self.ball_mi]
         targets.pop(self.car_index)
         return targets
 
@@ -187,7 +201,7 @@ class Visualizer:
         if self.input_dict.get(key, None) == "SWITCH_CAR" and is_pressed:
             if self.overwrite_controls:  # reset car controls before switching cars
                 self.arena.get_cars()[self.car_index].set_controls(RocketSim.CarControls())
-            self.car_index = (self.car_index + 1) % len(self.cars)
+            self.car_index = (self.car_index + 1) % len(self.cars_mi)
 
         if self.input_dict.get(key, None) == "TARGET_CAM" and is_pressed:
             self.target_cam = not self.target_cam
@@ -207,27 +221,27 @@ class Visualizer:
     def update_boost_pad_data(self):
         for i, pad in enumerate(self.arena.get_boost_pads()):
             pad_state = pad.get_state()
-            self.boost_pads[i].show() if pad_state.is_active else self.boost_pads[i].hide()
+            self.pads_mi[i].show() if pad_state.is_active else self.pads_mi[i].hide()
 
     def update_ball_data(self):
 
         # plot ball data
         ball_state = self.arena.ball.get_state()
 
+        # approx ball spin
+        ball_angvel_np = np.array([ball_state.ang_vel.x, -ball_state.ang_vel.y, ball_state.ang_vel.z])
+        rot_angle = np.linalg.norm(ball_angvel_np)
+        rot_axis = ball_angvel_np / max(1e-9, rot_angle)
+        delta_rot_angle = rot_angle * self.tick_skip / self.tick_rate
+
+        self.ball_mi.rotate(delta_rot_angle / math.pi * 180, *rot_axis, local=False)
+
         # location
-        ball_transform = self.ball.transform()
+        ball_transform = self.ball_mi.transform()
         ball_transform[0, 3] = -ball_state.pos.x
         ball_transform[1, 3] = ball_state.pos.y
         ball_transform[2, 3] = ball_state.pos.z
-        self.ball.setTransform(ball_transform)
-
-        # approx ball spin
-        ball_angvel_np = np.array([ball_state.ang_vel.x, ball_state.ang_vel.y, ball_state.ang_vel.z])
-        ball_delta_rot = ball_angvel_np * self.tick_skip / self.tick_rate
-
-        self.ball.rotate(ball_delta_rot[2] / math.pi * 180, 0, 0, -1, local=True)
-        self.ball.rotate(ball_delta_rot[0] / math.pi * 180, 0, 1, 0, local=True)
-        self.ball.rotate(ball_delta_rot[1] / math.pi * 180, 1, 0, 0, local=True)
+        self.ball_mi.setTransform(ball_transform)
 
         # ball ground projection
         self.ball_proj.resetTransform()
@@ -240,18 +254,18 @@ class Visualizer:
             car_state = car.get_state()
             car_angles = car_state.angles
 
-            self.cars[i].resetTransform()
+            self.cars_mi[i].resetTransform()
 
             # location
-            self.cars[i].translate(-car_state.pos.x, car_state.pos.y, car_state.pos.z)
+            self.cars_mi[i].translate(-car_state.pos.x, car_state.pos.y, car_state.pos.z)
 
             # rotation
-            self.cars[i].rotate(car_angles.yaw / math.pi * 180, 0, 0, -1, local=True)
-            self.cars[i].rotate(car_angles.pitch / math.pi * 180, 0, -1, 0, local=True)
-            self.cars[i].rotate(car_angles.roll / math.pi * 180, 1, 0, 0, local=True)
+            self.cars_mi[i].rotate(car_angles.yaw / math.pi * 180, 0, 0, -1, local=True)
+            self.cars_mi[i].rotate(car_angles.pitch / math.pi * 180, 0, -1, 0, local=True)
+            self.cars_mi[i].rotate(car_angles.roll / math.pi * 180, 1, 0, 0, local=True)
 
             # visual indicator for going supersonic
-            self.cars[i].opts["edgeColor"] = (0, 0, 0, 1) if car_state.is_supersonic else self.default_edge_color
+            self.cars_mi[i].opts["edgeColor"] = (0, 0, 0, 1) if car_state.is_supersonic else self.default_edge_color
 
     def update_camera_data(self):
 
@@ -273,7 +287,7 @@ class Visualizer:
             self.w.setCameraParams(azimuth=-target_azimuth / math.pi * 180,
                                    elevation=self.cam_dict["ANGLE"] - smaller_target_elevation / math.pi * 180)
 
-        if self.cars:
+        if self.cars_mi:
 
             car = self.arena.get_cars()[self.car_index]
             car_state = car.get_state()
@@ -281,10 +295,6 @@ class Visualizer:
             # center camera around the car
             self.w.opts["center"] = pg.Vector(-car_state.pos.x, car_state.pos.y,
                                               car_state.pos.z + self.cam_dict["HEIGHT"])
-
-            # debug info
-            self.text_item.text = f"{car_state.boost=:.1f}"
-            self.text_item.setParentItem(self.cars[self.car_index])
 
             if not self.target_cam:
                 # non-target_cam cam
@@ -294,16 +304,23 @@ class Visualizer:
                     self.w.setCameraParams(azimuth=-car_vel_azimuth / math.pi * 180,
                                            elevation=self.cam_dict["ANGLE"])
 
+    def update_text_data(self):
+        if self.cars_mi:
+            car_state = self.arena.get_cars()[self.car_index].get_state()
+            self.text_item.text = f"{car_state.boost=:.1f}"
+            self.text_item.setParentItem(self.cars_mi[self.car_index])
+
     def update_plot_data(self):
         self.update_boost_pad_data()
         self.update_ball_data()
         self.update_cars_data()
         self.update_camera_data()
+        self.update_text_data()
 
     def update(self):
 
         # only set car controls if overwrite_controls is true and there's at least one car
-        if self.overwrite_controls and self.cars:
+        if self.overwrite_controls and self.cars_mi:
             self.arena.get_cars()[self.car_index].set_controls(self.controls)
 
         # only call arena.step() if running in standalone mode
