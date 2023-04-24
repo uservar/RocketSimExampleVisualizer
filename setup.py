e5c36ae2a996a6db881a01da08a35c20594a5d4c
uservar <63248296+uservar@users.noreply.github.com>
2023-04-24 23:31:38 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.7",
     install_requires=requirements,
     package_data={'': ['rsvconfig-default.toml']},
     include_package_data=True
